cd20127ba4c1a8cab4fd09984c2b10b0a783eff7
Andre Luis Fernandes <andrerc0808@gmail.com>
2021-11-13 19:09:04 -0300

    gitflow-feature-stash: formulario_de_receita
    Adicionado criação de receita e visualização no dashboard do usuário.

--- receitas/models.py
+++ receitas/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from datetime import datetime
 
-from pessoas.models import Pessoa
-
+from django.contrib.auth.models import User
 
 class Receita(models.Model):
+    pessoa = models.ForeignKey(User, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
-    tempo_de_preparo = models.IntegerField()
+    tempo_preparo = models.IntegerField()
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
-    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y', blank=True)
     publicada = models.BooleanField(default=False)
 
